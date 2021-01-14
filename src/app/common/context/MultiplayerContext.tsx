ed38396bdfdf23103e5d889726b94fde471380f0
zenzillax <zenzillax@outlook.com>
2021-01-13 21:54:21 -0500

    refactor: multiplayer session

--- src/app/common/context/MultiplayerContext.tsx
+++ src/app/common/context/MultiplayerContext.tsx
@@ -5,14 +5,12 @@ import createReducer from "../reducer";
 import { handlers } from "../reducer/handlers";
 import WebSocketClient from "../WebSocketClient";
 import { useRef } from "react";
+import SessionContextProvider, { SessionContext } from "./SessionContext";
 
 export const MultiplayerContext = createContext<any>({});
 
 export interface MultiplayerContextProvider {
   state: any;
-  updateProp: (path: string, value: any) => void | any;
-  deleteProp: (path: string) => void | any;
-  reset: (value: any) => void | any;
   sendRequest: (action: string, data?: any) => void;
 }
 
@@ -22,44 +20,14 @@ export default function MultiplayerContextProvider(props) {
   const localhost = isEmulator ? "10.0.2.2" : "localhost";
   const initialState = {
     serverUrl: `ws://${localhost}:3000`,
-    webSocket: new WebSocket(`ws://${localhost}:3000`),
-    sessions: {}
+    webSocket: new WebSocket(`ws://${localhost}:3000`)
   };
-  const reducer = createReducer(initialState, handlers);
-  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const [state] = useState(initialState);
 
   // need to keep track of the latest state to prevent stale values from being read in function closures
   const _state = useRef(null);
   _state.current = state;
-
-  function updateProp(path: string, value: any) {
-    // updates the state in the store
-    dispatch({
-      type: "updateProp",
-      path,
-      value
-    });
-    // this does not update the state in the store but returns what the next state looks like
-    // this is to bypass waiting for the next app render to get the latest state
-    return _.set(state, path, value);
-  }
-
-  function deleteProp(path: string) {
-    dispatch({
-      type: "deleteProp",
-      path,
-      value: null
-    });
-    return _.unset(state, path);
-  }
-
-  function reset(value: any) {
-    dispatch({
-      type: "reset",
-      path: null,
-      value
-    });
-  }
 
   function sendRequest(action: string, data: any = {}) {
     const json = {
@@ -71,15 +39,14 @@ export default function MultiplayerContextProvider(props) {
 
   const value: MultiplayerContextProvider = {
     state,
-    updateProp,
-    deleteProp,
-    reset,
     sendRequest
   };
 
   return (
     <MultiplayerContext.Provider value={value}>
-      <WebSocketClient webSocket={state.webSocket}>{props.children}</WebSocketClient>
+      <SessionContextProvider>
+        <WebSocketClient webSocket={state.webSocket}>{props.children}</WebSocketClient>
+      </SessionContextProvider>
     </MultiplayerContext.Provider>
   );
 }
--- src/app/views/multiplayer/screens/MultiplayerHomeScreen.tsx
+++ src/app/views/multiplayer/screens/MultiplayerHomeScreen.tsx
@@ -1,60 +1,118 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { getStatusBarHeight } from "react-native-status-bar-height";
-import { SafeAreaView, StyleSheet, Text, View, Button } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View, Button, TextInput } from "react-native";
 import { SectionList } from "react-native";
 import { MultiplayerContext, MultiplayerContextProvider } from "../../../common/context/MultiplayerContext";
 import { AppContext, AppContextProvider } from "../../../common/context/AppContext";
 import { FlatList } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+import { SessionContext, SessionContextProviderValue } from "../../../common/context/SessionContext";
 
-const Item = ({ title, navigation }) => (
-  <View style={{ paddingTop: 16 }}>
-    <Button title={title} onPress={() => {navigation.navigate("FactionSelectionScreen")}}></Button>
-  </View>
-);
-
-export default function MultiplayerHomeScreen() {
-  const { state, sendRequest } = useContext(MultiplayerContext) as MultiplayerContextProvider;
-  const { uniqueId } = useContext(AppContext) as AppContextProvider;
+function Item({ session, player }) {
+  const navigation = useNavigation();
+  const { sendRequest } = useContext(MultiplayerContext) as MultiplayerContextProvider;
+  const { updateProp } = useContext(SessionContext) as SessionContextProviderValue;
+  const sessionId = session.id;
 
   return (
-    <>
-      <SafeAreaView style={{ flex: 1 }}>
+    <View style={{ paddingTop: 16 }}>
+      <Button
+        title={sessionId}
+        onPress={() => {
+          sendRequest("ADD_PLAYER_TO_GAME_SESSION", {
+            sessionId: sessionId,
+            playerId: player.id,
+            name: player.name
+          });
+          updateProp("player.sessionId", sessionId);
+          navigation.navigate("SessionScreen", { sessionId, playerId: player.id, name: player.name });
+        }}></Button>
+    </View>
+  );
+}
+
+export default function MultiplayerHomeScreen() {
+  const { state: multiplayerState, sendRequest } = useContext(MultiplayerContext) as MultiplayerContextProvider;
+  const { state: sessionState, updateProp } = useContext(SessionContext) as SessionContextProviderValue;
+  const navigation = useNavigation();
+  const [_sessions, _setSessions] = useState([]);
+
+  useEffect(() => {
+    if (sessionState["sessions"]) {
+      const sessions = Object.entries(sessionState["sessions"]).map(([key, value]: [string, any]) => {
+        return {
+          id: key,
+          data: value
+        };
+      });
+      _setSessions(sessions);
+    }
+  }, [sessionState["sessions"]]);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View
+        style={{
+          paddingTop: 16,
+          paddingHorizontal: 16
+        }}>
+        <Text style={{ fontSize: 24, alignSelf: "center" }}>Game Sessions</Text>
+        <View style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+          <Text>
+            <Text style={{ fontSize: 20 }}>Server: </Text>
+            <Text style={{ fontSize: 16 }}>{multiplayerState.serverUrl}</Text>
+          </Text>
+        </View>
         <View
           style={{
-            paddingTop: 16,
-            paddingLeft: 16
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            paddingTop: 16
           }}>
-          <Text style={{ fontSize: 24, alignSelf: "center" }}>Local Games</Text>
-          <View style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-            <Text>
-              <Text style={{ fontSize: 20 }}>Server: </Text>
-              <Text style={{ fontSize: 16 }}>{state.serverUrl}</Text>
-            </Text>
-          </View>
-        </View>
-
-        <View style={styles.container}>
-          <FlatList
-            data={state.sessions}
-            keyExtractor={(item: any) => item.id}
-            renderItem={({ item }) => <Item title={item.name} navigation={props.navigation} />}
+          <Text style={{ fontSize: 20, justifyContent: "center" }}>Player Name: </Text>
+          <TextInput
+            style={{ height: 40, borderColor: "gray", borderWidth: 1, flexGrow: 1 }}
+            onChangeText={(text) => updateProp("player.name", text)}
+            value={sessionState.player.name}
           />
         </View>
-        <View style={{ paddingHorizontal: 16, paddingBottom: 16 }}>
-          <Button
-            title="Create Session"
-            onPress={() =>
-              sendRequest("CREATE_GAME_SESSION", { sessionId: uniqueId, name: uniqueId.substr(0, 7).toUpperCase() })
-            }></Button>
-        </View>
-        <View style={{ paddingHorizontal: 16, paddingBottom: 16 }}>
-          <Button title="Direct Connect" onPress={() => {}}></Button>
-        </View>
-        <View style={{ paddingHorizontal: 16, paddingBottom: 16 }}>
-          <Button title="Refresh" onPress={() => sendRequest("GET_GAME_SESSIONS")}></Button>
-        </View>
-      </SafeAreaView>
-    </>
+      </View>
+
+      <View style={styles.container}>
+        <FlatList
+          data={_sessions}
+          keyExtractor={(session: any) => session.id}
+          renderItem={({ item }) => <Item session={item} player={sessionState.player} />}
+        />
+      </View>
+      <View style={{ paddingHorizontal: 16, paddingBottom: 16 }}>
+        <Button
+          title="Create Session"
+          onPress={() => {
+            sendRequest("CREATE_GAME_SESSION", { sessionId: sessionState.player.id, name: sessionState.player.name });
+            sendRequest("ADD_PLAYER_TO_GAME_SESSION", {
+              sessionId: sessionState.player.id,
+              playerId: sessionState.player.id,
+              name: sessionState.player.name
+            });
+            updateProp("player.sessionId", sessionState.player.id);
+            navigation.navigate("SessionScreen", {
+              sessionId: sessionState.player.id,
+              playerId: sessionState.player.id,
+              name: sessionState.player.name
+            });
+          }}
+        />
+      </View>
+      <View style={{ paddingHorizontal: 16, paddingBottom: 16 }}>
+        <Button title="Direct Connect" onPress={() => {}}></Button>
+      </View>
+      <View style={{ paddingHorizontal: 16, paddingBottom: 16 }}>
+        <Button title="Refresh" onPress={() => sendRequest("GET_GAME_SESSIONS")}></Button>
+      </View>
+    </SafeAreaView>
   );
 }
 
